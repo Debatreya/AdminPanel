0f52963389ea2303ebb9f0e9a80b9a15794356c6
Debatreya <debatreyadas@gmail.com>
2026-01-11 16:29:23 +0530

    Added IMGBB Uploading function in utils

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,38 @@ export interface SelectOption {
   value: string;
 }
 
+// ImgBB Types
+export interface ImgBBImageData {
+  filename: string;
+  name: string;
+  url: string;
+}
+
+export interface ImgBBUploadResponse {
+  data: {
+    id: string;
+    title: string;
+    url_viewer: string;
+    url: string;
+    display_url: string;
+    width: string;
+    height: string;
+    size: string;
+    time: string;
+    expiration: string;
+    image: ImgBBImageData;
+    delete_url: string;
+  };
+  success: boolean;
+  status: number;
+}
+
+export interface ImgBBUploadResult {
+  url: string;
+  deleteUrl: string;
+  imageData: ImgBBImageData;
+}
+
 export interface TableColumn<T = any> {
   key: keyof T;
   label: string;
